61e7a97ac96fd1eac6212dba156cc6a778e5da8f
JBourne <jayelbourne@gmail.com>
2014-10-17 19:31:53 -0400

    Add profile_names to users

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -3,4 +3,8 @@ class Sheet < ActiveRecord::Base
 
     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+    validates :image, presence: true
+    validates :description, presence: true
+
 end
---
+++ db/migrate/20141017192714_add_profile_names_to_users.rb
@@ -0,0 +1,5 @@
+class AddProfileNamesToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :profile_name, :string
+  end
+end
