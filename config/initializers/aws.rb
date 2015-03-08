3b0152a4df6b17594cc155bfb6d316cabb13b01f
JElbourne <jayelbourne@gmail.com>
2015-03-07 21:34:12 -0500

    Update AWS initializer, commented it out for deploy test

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,7 @@
 
 #credentials = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-s3 = Aws::S3::Resource.new
-S3_BUCKET = s3.bucket(ENV['S3_BUCKET'])
+#s3 = Aws::S3::Resource.new
+#S3_BUCKET = s3.bucket(ENV['S3_BUCKET'])
 
 # Aws.config({
 #   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
