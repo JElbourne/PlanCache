0a6027c4f1cff1cdc4a174321d6db01bd24f0efb
JElbourne <jayelbourne@gmail.com>
2015-03-11 01:02:59 -0400

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount_griddler
   resources :users
-  #root to: 'welcome#index'
+  root to: 'welcome#index'
   #post '/email_processor' => 'griddler/emails#create'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
