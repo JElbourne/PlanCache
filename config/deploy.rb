c2471ab379d27ba49da4b5c19cf1a99da6a71d06
JElbourne <jayelbourne@gmail.com>
2015-03-13 12:21:28 -0400

    Updated to run tests at deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,26 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 after 'deploy:publishing', 'deploy:restart'
 after "deploy:restart", "deploy:cleanup"
 
+set :test_log, "log/capistrano.test.log"
+
 namespace :deploy do
+  desc "Runs test before deploying, can't deploy unless they pass"
+  task :run_tests do
+    test_log = "log/capistrano.test.log"
+    tests = fetch(:tests)
+    tests.each do |test|
+      puts "--> Running tests: '#{test}', please wait ..."
+      unless system "bundle exec rspec #{test} > #{test_log} 2>&1"
+        puts "--> Tests: '#{test}' failed. Results in: #{test_log} and below:"
+        system "cat #{test_log}"
+        exit;
+      end
+      puts "--> '#{test}' passed"
+    end
+    puts "--> All tests passed"
+    system "rm #{test_log}"
+  end
+  
   task :restart do
     invoke 'unicorn:reload'
   end
